f876414f6aa07b321f955f4ae71f6f0251a32201
suh-lab <yun.su.hwan27@gmail.com>
2021-05-15 23:01:31 +0900

    update

---
+++ 21_May/15.py
@@ -0,0 +1,58 @@
+#Question 251
+# class Human():
+#     pass
+
+#Question 252
+# class Human():
+#     pass
+
+# areum = Human()
+
+#Question 254
+# class Human():
+#     def __init__(self):
+#         print("응애응애")
+
+# Human()
+
+#Question 255
+# class Human():
+#     def __init__(self, name, age, gender):
+#         print(name, age, gender)
+
+# Human("아름", 25, "여자")
+
+#Question 256
+# class Human():
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#         print(f"이름 : {name}, 나이 : {age}, 성별 : {gender}")
+
+# test = Human(1, 2, 3)
+# print(test.age)
+
+#Question 257
+# class Human():
+#     def __init__(self, name, age, gender ):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+
+#     def who(self):
+#         print(f"이름 : {self.name}, 나이 : {self.age}, 성별 : {self.gender}")
+
+# Human(1, 2, 3).who()
+# test = Human(1, 2, 3)
+# test.who()
+
+#Question 258
+# class Human():
+#     def __init__(self):
+#         print("테스트")
+    
+#     def __del__(self):
+#         print("삭제")
+
+# test = Human()
